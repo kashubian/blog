4f657fd13605c69251c702faa0c63b34d3d3f212
kashubian <karalusmichal@gmail.com>
2023-03-27 22:26:44 +0200

    Cleanup

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,16 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
-
 export function App() {
-  return (
-    <>
-      <NxWelcome title="frontend" />
-
-      <div />
-    </>
-  );
+  return <div>blog</div>;
 }
 
 export default App;
