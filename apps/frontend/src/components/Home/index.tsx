19c4cac8dfbe2e19ba6d2b50e500ea1324669f8b
kashubian <karalusmichal@gmail.com>
2023-04-04 21:09:21 +0200

    Add usePosts hook

--- apps/frontend/src/components/Home/index.tsx
+++ apps/frontend/src/components/Home/index.tsx
@@ -1,42 +1,33 @@
-import { useEffect, useState } from 'react';
-
-import { API_URL } from '../../constants';
-import Post from '../../types/post';
 import useDocumentTitle from '../../hooks/useDocumentTitle';
 import Container from '../common/Container';
+import { usePosts } from '../../hooks/usePosts';
 
 function Home() {
-  const [posts, setPosts] = useState<Post[]>([]);
-
+  const { data } = usePosts('sort=publishedAt:DESC');
   useDocumentTitle('Blog');
-
-  useEffect(() => {
-    fetch(`${API_URL}/api/posts?sort=publishedAt:DESC`)
-      .then((response) => response.json())
-      .then((body) => setPosts(body.data));
-  }, []);
 
   return (
     <Container>
-      {posts.map((post) => {
-        return (
-          <article key={post.id}>
-            <div className="row">
-              <div className="col-9">
-                <time dateTime={post.attributes.publishedAt}>
-                  {post.attributes.publishedAt}
-                </time>
-                <h2>{post.attributes.title}</h2>
-                <p>
-                  {post.attributes.description.substring(0, 109)}
-                  {post.attributes.description.length >= 109 && '...'}
-                </p>
+      {data &&
+        data.map(({ id, attributes }) => {
+          return (
+            <article key={id}>
+              <div className="row">
+                <div className="col-9">
+                  <time dateTime={attributes.publishedAt}>
+                    {attributes.publishedAt}
+                  </time>
+                  <h2>{attributes.title}</h2>
+                  <p>
+                    {attributes.description.substring(0, 300)}
+                    {attributes.description.length >= 300 && '...'}
+                  </p>
+                </div>
+                <div className="col-3">TODO add image</div>
               </div>
-              <div className="col-3">TODO add image</div>
-            </div>
-          </article>
-        );
-      })}
+            </article>
+          );
+        })}
     </Container>
   );
 }
