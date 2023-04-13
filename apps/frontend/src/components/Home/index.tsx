26a22e9138d8b746b1352d07487464a7cd44e5e3
kashubian <karalusmichal@gmail.com>
2023-04-13 18:22:20 +0200

    Move posts to separate component

--- apps/frontend/src/components/Home/index.tsx
+++ apps/frontend/src/components/Home/index.tsx
@@ -1,62 +1,14 @@
-import format from 'date-fns/format';
-import { Link } from 'react-router-dom';
-
 import useDocumentTitle from '../../hooks/useDocumentTitle';
 import Container from '../common/Container';
-import { usePosts } from '../../hooks/usePosts';
-import { API_URL } from '../../constants';
 
-import styles from './Home.module.scss';
+import PostsList from '../PostsList';
 
 function Home() {
-  const { data } = usePosts('sort=publishedAt:DESC&populate=photo');
   useDocumentTitle('Blog');
 
   return (
     <Container>
-      {data &&
-        data.map(({ id, attributes }) => {
-          return (
-            <article className={styles.article} key={id}>
-              <div className="row">
-                <div className="col-8">
-                  <time
-                    className={styles.publishedAt}
-                    dateTime={attributes.publishedAt}
-                  >
-                    {format(new Date(attributes.publishedAt), 'PP')}
-                  </time>
-                  <h2 className={styles.heading}>{attributes.title}</h2>
-                  <p className={styles.description}>
-                    {attributes.description.substring(0, 300)}
-                    {attributes.description.length >= 300 && '...'}
-                  </p>
-                </div>
-                <div className="col-4">
-                  <div className={styles.photo}>
-                    <img
-                      className={styles.photo__wrapper}
-                      src={API_URL + attributes.photo.data.attributes.url}
-                      alt=""
-                    />
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col">
-                    <div className={styles.articleInfo}>
-                      <Link to="/" className={styles.label}>
-                        {attributes.label}
-                      </Link>
-                      <div className={styles.readingTime}>
-                        {attributes.estimatedReadingTime} min read
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </article>
-          );
-        })}
+      <PostsList />
     </Container>
   );
 }
