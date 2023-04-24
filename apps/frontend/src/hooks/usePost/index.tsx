1002ca21397e82729ff4ab2d2bf481bdba1447f1
kashubian <karalusmichal@gmail.com>
2023-04-24 19:49:00 +0200

    Add usePost hook

---
+++ apps/frontend/src/hooks/usePost/index.tsx
@@ -0,0 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { API_URL } from '../../constants';
+
+export const usePost = (id: string) => {
+  const apiUrl = `${API_URL}/api/posts/${id}`;
+
+  return useQuery(['post', id], () =>
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((body) => body.data)
+  );
+};
